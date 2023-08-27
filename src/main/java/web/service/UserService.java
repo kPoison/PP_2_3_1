03c8a4e7be1b399b0ee4d290b2a5b8e9494d4057
kPoison <yakarnapelev@gmail.com>
2023-08-27 09:22:29 +0300

    Update

--- src/main/java/web/service/UserService.java
+++ src/main/java/web/service/UserService.java
@@ -5,10 +5,9 @@ import web.model.User;
 import java.util.List;
 
 public interface UserService {
-
-    public List<User> getAll();
-    public User getById(Long id);
-    public void save(User user);
-    public void update(User user);
-    public void delete(Long id);
+    List<User> getAll();
+    User getById(Long id);
+    void save(User user);
+    void update(User user);
+    void delete(Long id);
 }
